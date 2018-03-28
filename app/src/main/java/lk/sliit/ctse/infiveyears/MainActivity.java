109b26b13a5c8760037238f0b62fc589223f8b1a
Lanil <lanil.marasinghe@pearson.com>
2018-03-29 00:43:43 +0530

    Completed some question activity

--- app/src/main/java/lk/sliit/ctse/infiveyears/MainActivity.java
+++ app/src/main/java/lk/sliit/ctse/infiveyears/MainActivity.java
@@ -2,10 +2,13 @@ package lk.sliit.ctse.infiveyears;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import lk.sliit.ctse.infiveyears.data.Question;
 import lk.sliit.ctse.infiveyears.data.QuestionData;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     // UI Elements
     TextView tv_questionText;
     TextView tv_qIndex;
+    EditText et_answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +43,27 @@ public class MainActivity extends AppCompatActivity {
         // Initializing UI elements
         tv_questionText = findViewById(R.id.tv_questionText);
         tv_qIndex = findViewById(R.id.tv_qIndex);
+        et_answer = findViewById(R.id.et_answer);
+
+        this.getNextQuestion(null);
 
     }
 
     public void getNextQuestion(View view){
+        String answer = et_answer.getText().toString();
+        if(questionData.getQuestionIndex() > 0 && answer.matches("")){
+            Toast.makeText(MainActivity.this, "We need the answer !", Toast.LENGTH_SHORT).show();
+            return;
+        }
         if(questionData.hasNext()){
+            et_answer.setText("");
             Question question = questionData.getNextQuestion();
             tv_questionText.setText(question.getQuestion());
             String qIndex = questionData.getQuestionIndex() + " / 10";
             tv_qIndex.setText(qIndex);
+
+            View currentView = findViewById(R.id.activity_main);
+            currentView.setBackgroundColor(Color.parseColor(question.getColor()));
         }else{
             this.openCompleteActivity(view);
         }
